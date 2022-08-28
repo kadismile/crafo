c88fcbd7dac1208ddd7f016a53b1e8d2d99d42f9
Orkhai <dicksonorkhai@gmail.com>
2022-08-28 20:29:47 +0100

    Added carousel slider

--- assets/carousel.js
+++ assets/carousel.js
@@ -4,6 +4,7 @@ showSlides();
 function showSlides() {
   let i;
   let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
@@ -11,6 +12,10 @@ function showSlides() {
   if (slideIndex > slides.length) {
     slideIndex = 1;
   }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
   slides[slideIndex - 1].style.display = "block";
-  setTimeout(showSlides, 5000); // Change image every 5 seconds
+  dots[slideIndex - 1].className += " active";
+  setTimeout(showSlides, 3000); // Change image every 3 seconds
 }
